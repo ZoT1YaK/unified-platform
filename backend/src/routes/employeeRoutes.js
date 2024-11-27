a20ff46dd87e0a9704e8a7c847e29d9618a67097
Roberts Zustars <robert.zustars@gmail.com>
2024-11-27 19:19:14 +0100

    Update employee model, controller,  routes. Add functionality to get employee info

--- backend/src/routes/employeeRoutes.js
+++ backend/src/routes/employeeRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const { loginEmployee } = require("../controllers/employeeController");
+const { verifyToken, verifyPeopleLeader } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 // Login route
 router.post("/login", loginEmployee);
+
+// Profile route
+router.get("/profile", verifyToken, getProfile);
 
 module.exports = router;

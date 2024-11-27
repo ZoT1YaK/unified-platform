fefeefdcedd7f45f9f22b590e1a60994157df098
Roberts Zustars <robert.zustars@gmail.com>
2024-11-27 19:20:07 +0100

    Add department location model

---
+++ backend/src/models/Location.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const locationSchema = new mongoose.Schema({
+  dep_num: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Department", 
+    required: true 
+  },
+  country: { 
+    type: String, 
+    required: true 
+  },
+  city: { 
+    type: String, 
+    required: true 
+  },
+  zip: { 
+    type: String, 
+    required: true 
+  },
+  street: { 
+    type: String, 
+    required: true 
+  },
+});
+
+module.exports = mongoose.model("Location", locationSchema);
